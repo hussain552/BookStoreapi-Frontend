2660836a1d8c64e7cd73db543a282560ff1090c6
Ahamed Hussaini Pasha Shaik <shahidshaik372@gmail.com>
2024-10-24 19:52:27 +0530

    hussain

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Use the PORT env variable or default to 3000
+    host: '0.0.0.0', // Listen on all interfaces
+  },
+});
